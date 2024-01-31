5529b7ddc07acf0166f2406d9871ef66b25ba282
Resheba <74815401+Resheba@users.noreply.github.com>
2024-01-31 05:53:05 +0300

    rabbitmq recode

--- rabbitmq/client.py
+++ rabbitmq/client.py
@@ -14,13 +14,7 @@ class RabbitClient:
 
     @classmethod
     def start(cls) -> None:
-        cls._connection: BlockingConnection = BlockingConnection(
-            [
-                URLParameters(url=Settings.RABBITMQ_DSN),
-                ConnectionParameters(heartbeat=10)
-            ]
-        )
-        cls._channel: BlockingChannel = cls._connection.channel()
+        cls._connect()
         cls._channel.exchange_declare(exchange=cls._message_topic_name, exchange_type=ExchangeType.topic)
         cls._channel.queue_declare(queue=cls._message_queue_name)
         cls._channel.queue_bind(queue=cls._message_queue_name, exchange=cls._message_topic_name, routing_key='user.*')
@@ -28,17 +22,19 @@ class RabbitClient:
     @classmethod
     def publish(cls, message: str, *, user_id: int | str) -> None:
         try:
-            cls._channel.basic_publish(exchange=cls._message_topic_name,
-                                    routing_key=f'user.{user_id}',
-                                    body=message)
+            cls._send(message=message, user_id=user_id)
         except StreamLostError:
-            cls.reconnect()
-            cls._channel.basic_publish(exchange=cls._message_topic_name,
+            cls._connect()
+            cls._send(message=message, user_id=user_id)
+    
+    @classmethod
+    def _send(cls, message: str, user_id: int | str) -> None:
+        cls._channel.basic_publish(exchange=cls._message_topic_name,
                                     routing_key=f'user.{user_id}',
                                     body=message)
-        
+
     @classmethod
-    def reconnect(cls) -> None:
+    def _connect(cls) -> None:
         cls._connection: BlockingConnection = BlockingConnection(
             [
                 URLParameters(url=Settings.RABBITMQ_DSN),
